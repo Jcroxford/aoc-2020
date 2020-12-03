06a84dbe2d09e9bf9413f54b5aaa3aba5ae6a8aa
Jesse Croxford <jcroxford@simplenexus.com>
2020-12-02 22:40:51 -0700

    index.js can now be called to run methods for a given part and day

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log(process.argv.slice)
+// when calling index provide args for day (1-24) and part (1 or 2)
+const [_, __, day, part] = process.argv
+
+const scope = require(`./${day}/part${part}.js`)
+
+console.log(Number(part) == 1 ? scope.part1() : scope.part2())
