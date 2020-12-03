e02f92df95777856229748804fb339b92f1ca30e
Jesse Croxford <jcroxford@simplenexus.com>
2020-12-02 21:56:53 -0700

    update import data to use __dirname

--- utils/importData.js
+++ utils/importData.js
@@ -5,6 +5,6 @@ module.exports = {
    * @param {number} day
    */
   pullDataForDay(day) {
-    return fs.readFileSync(`../${day}/input.txt`, 'utf-8')
+    return fs.readFileSync(`${__dirname}/../${day}/input.txt`, 'utf-8')
   }
 }
